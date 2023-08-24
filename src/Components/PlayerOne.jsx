bc7574c1e351e071cdcf212ea45d74d3e71d297b
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-08-24 19:08:11 -0400

    component set up

---
+++ src/Components/PlayerOne.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayerOne = () => {
+    return (
+        <div>
+            <h3>Player One</h3>
+            <h4>Hitpoints: </h4>
+        </div>
+    );
+}
+
+export default PlayerOne;
